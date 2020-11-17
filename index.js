c88367326718d600ae5086bb88f29e8813eb02cc
Peter Morrow <peterjm@live.unc.edu>
2020-11-17 18:39:55 -0500

    Fixed error

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path')
 const app = express();
-require('./database');
 
 app.use(bodyParser.json());
 app.use(cors());
