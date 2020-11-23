7e42a9be153b5d216e4daef4af9d5e086d274df6
Peter Morrow <peterjm@live.unc.edu>
2020-11-22 20:23:44 -0500

    changed sameSite setting

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.enable('trust proxy');
 app.use(expressSession({
     name: "battleshipComp426",
     secret: "express session secret",
-    cookie: { secure: false},
+    cookie: { secure: false, sameSite: 'none'},
     resave: false,
     saveUninitialized: true,
     store: new MongoStore({url: 'mongodb+srv://battleship:comp426@cluster0.fqc7a.mongodb.net/battleship?retryWrites=true&w=majority'})
